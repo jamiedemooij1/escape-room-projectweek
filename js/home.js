e185e00e634f7897ef28df33bcc5e9a84c5f9908
jamiedemooij <jamiedemooij@gmail.com>
2022-04-13 10:46:50 +0200

    cadeautje
    a

--- js/home.js
+++ js/home.js
@@ -1,7 +1,1 @@
-const startButton = document.querySelector('.start');
-
-if(startButton) {
-    startButton.addEventListener('click', function() {
-        location.replace('index.html');
-    })
-}
+volgende keer uitloggen sukkel
